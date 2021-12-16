f1ae506aaeddfad12bd33ace24d88abff4602cbe
toan <dhh51805788@student.stu.edu.vn>
2021-12-16 09:17:11 +0700

    them code vao file slidebar.php

---
+++ layout/slidebar.php
@@ -0,0 +1,26 @@
+<div class="single-sidebar none">
+                        <h2 class="sidebar-title">Máy Bộ</h2>
+                        <?php
+                             for($i=0;$i<5;$i++){
+                                echo '<div class="thubmnail-recent">
+                                <img src="img/pc-streamer1.jpg" class="recent-thumb" alt="">
+                                <h2><a href="">PC Streamer 1</a></h2>
+                                <div class="product-sidebar-price">
+                                    <ins>25.000.000đ</ins> <del>30.000.000đ</del>
+                                </div>                             
+                            </div>';
+                            }
+                        ?>
+                        
+                    </div>
+                    
+                    <div class="single-sidebar none">
+                        <h2 class="sidebar-title">Tin Tức</h2>
+                        <ul class="reset">
+                        <?php
+                             for($i=0;$i<5;$i++){
+                                echo '<li><a href="">Cần tuyển nhân viên giữ xe</a></li>';
+                            }
+                        ?>            
+                        </ul>
+                    </div>
