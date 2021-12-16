da500629fd42c112c414240424a09310b601cfe8
toan <dhh51805788@student.stu.edu.vn>
2021-12-16 10:02:46 +0700

    them code vao file ds-sp.php

---
+++ admin/ds-sp.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html class="no-js h-100" lang="en">
+  <head>
+    <?php 
+    include 'layout/check.php';
+    include 'layout/head.php';?>
+  </head>
+  <body class="h-100">
+        <?php 
+    // include 'layout/accent-color.php';
+    ?>
+    <div class="container-fluid">
+      <div class="row">
+        <!-- Main Sidebar -->
+        <?php include 'layout/sidebar.php';?>
+        <!-- End Main Sidebar -->
+        <main class="main-content col-lg-10 col-md-9 col-sm-12 p-0 offset-lg-2 offset-md-3">
+          <div class="main-navbar sticky-top bg-white">
+            <!-- Main Navbar -->
+            <?php include 'layout/navbar.php';?>
+            <!-- / .main-navbar -->
+          </div>
+          <div class="main-content-container container-fluid px-4">
+            <!-- Page Header -->
+            <div class="page-header row no-gutters py-4">
+              <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
+                <span class="text-uppercase page-subtitle">Quản lý</span>
+                <h3 class="page-title">Sản Phẩm</h3>
+              </div>
+            </div>
+            <!-- End Page Header -->
+              <div class="col-12">
+                <div class="card card-small mb-4">
+                  <div class="card-header border-bottom">
+                    <div class="row">
+                      <div class="col-md-6">
+                        <h6 class="m-0">Danh sách sản phẩm</h6>
+                      </div>
+                      <div class="col-md-6" style="text-align:right">
+                        <a href="them-sp.php" class="mb-2 btn btn-success mr-2 btn-font" style="margin:0 !important;"><i class="fas fa-plus"></i> Thêm</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="table-sp">
+                      <div class="card-body p-0 pb-3 text-center">
+                          <table class="table mb-0">
+                            <thead class="bg-light">
+                              <tr>
+                                <th scope="col" class="border-0">Mã sản phẩm</th>
+                                <th scope="col" class="border-0">Hình ảnh</th>
+                                <th scope="col" class="border-0">Tên sản phẩm</th>
+                                <th scope="col" class="border-0">Loại</th>
+                                <th scope="col" class="border-0 none">Hãng</th>
+                                <th scope="col" class="border-0 none">Bảo hành</th>
+                                <th scope="col" class="border-0">Số lượng (Cái)</th>
+                                <th scope="col" class="border-0">Giá (đ)</th>
+                                <th scope="col" class="border-0">Tùy chỉnh</th>
+                              </tr>
+                            </thead>
+                            <tbody><?php
+                              $arrMaSP=array("SP01","SP02","SP03");
+                              $arrTenHinh=array("pc-gaming","pc-streamer","laptop-3");
+                              $arrTenSP=array("PC Gaming","PC Streamer","Laptop Lập Trình");
+                              $arrLoai=array("PC","PC","Laptop");
+                              $arrHang=array("T&T","T&T","Asus");
+                              $arrBaoHanh=array(6,12,24);
+                              $arrSoLuong=array(10,1,100);
+                              $arrGia=array(10000000,25000000,9999999);
+                              for($i=0;$i<count($arrMaSP);$i++) {
+                                echo '
+                                <tr>
+                                <td>'.$arrMaSP[$i].'</td>
+                                <td><img class="hinh-anh" src="../img/'.$arrTenHinh[$i].'.jpg"></td>
+                                <td>'.$arrTenSP[$i].'</td>
+                                <td>'.$arrLoai[$i].'</td>
+                                <td class="none">'.$arrHang[$i].'</td>                          
+                                <td class="none">'.$arrBaoHanh[$i].' tháng</td>                          
+                                <td>'.$arrSoLuong[$i].'</td>                          
+                                <td>'.number_format($arrGia[$i],0,",",".").'</td>                          
+                                <td>
+                                <a href="sua-sp.php" class="mb-2 btn btn-info mr-2 btn-font">
+                                  <i class="fas fa-edit"></i><p>&nbspSửa</p>
+                                </a>
+                                <button type="button" class="mb-2 btn btn-danger mr-2 btn-font">
+                                  <i class="fas fa-times"></i><p>&nbspXóa</p>                                  
+                                </button>
+                                </td>                          
+                                </tr>
+                                ';
+                              }
+                              ?></tbody>
+                          </table>
+                        </div>
+                  </div>
+                </div>
+              </div>
+          </div>
+          <?php include 'layout/footer.php';?>
+        </main>
+      </div>
+    </div>
+<?php include 'layout/script.php';?>
+  </body>
+</html>
